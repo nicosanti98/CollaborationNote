6b79c6484c61467534a93e588298d825b25d33b3
nicosanti98 <48328201+nicosanti98@users.noreply.github.com>
2021-11-16 19:29:30 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     globalObject: 'self',
-    path: path.resolve(__dirname, '/dist/'),
+    path: path.resolve(__dirname, './dist/'),
     filename: '[name].bundle.js',
     publicPath: '/codemirror/dist/'
   },
