129fdb1c9d91fad394a4222753051a0e772c90b9
nicosanti98 <48328201+nicosanti98@users.noreply.github.com>
2021-11-25 18:05:36 +0100

    Update codemirror.js

--- codemirror.js
+++ codemirror.js
@@ -23,7 +23,7 @@ window.addEventListener('load', async () => {
     const ydoc = new Y.Doc()
     //Creazione collegamento tra peer basato su stesso nome stanza
     const provider = new WebsocketProvider(
-        'wss://collaborationnote.herokuapp.com/codemirror',
+        'ws://localhost:1234',
         params.room,
         ydoc
 
